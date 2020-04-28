adfa5904f9aa545705247732d59f92ba378bec11
Kuro <emilemunch@hotmail.fr>
2020-04-28 18:16:58 +0200

    minor changes

--- src/js/bonuses.js
+++ src/js/bonuses.js
@@ -42,116 +42,120 @@ function bonus(){
   }
 
  function apply_bonus(bonus){
-    switch(bonus){
-        case bonuses.DA_WALL:
-          console.log("DA WALLU");
-          currBonus = bonuses.DA_WALL;
-          confine();
-          document.getElementById("info").textContent="You are now under lockdown for 5s";
-          if(play){
-            yareyaredaze.play();
-          }
-          
-        break;
-        case bonuses.ENEMY_HEAL:
-            if(play){
-                ohno.play();
-            }
-          currBonus = bonuses.ENEMY_HEAL;
-          document.getElementById("info").textContent="Oh no! ennemy healed 1HP :("
-          console.log("da bad healu");
-          if(botH<3 && botH >0){
-            botH++;
-            switch(botH){
-              case 2:
-                botUI.childNodes[1].style.background = "red";
-                botUI.childNodes[3].style.background = "cadetblue";
-                botUI.childNodes[5].style.background = "cadetblue";
-              break;
-              case 3:
-                botUI.childNodes[1].style.background = "cadetblue";
-                botUI.childNodes[3].style.background = "cadetblue";
-                botUI.childNodes[5].style.background = "cadetblue";
-              break;
-            }
-          }
-        break;
-        case bonuses.SELF_HEAL:
-            if(play){
+
+    if(isPlaying){
+        switch(bonus){
+            case bonuses.DA_WALL:
+              console.log("DA WALLU");
+              currBonus = bonuses.DA_WALL;
+              confine();
+              document.getElementById("info").textContent="You are now under lockdown for 5s";
+              if(play){
                 yareyaredaze.play();
               }
-          currBonus = bonuses.SELF_HEAL;
-          document.getElementById("info").textContent="Congratulations: you healed 1HP :)";
-          console.log("da good healu");
-          if(playerH< 3 && playerH>0){
-            playerH++;
-            switch (playerH){
-              case 2:
-                playerUI.childNodes[1].style.background = "red";
-                playerUI.childNodes[3].style.background = "cadetblue";
-                playerUI.childNodes[5].style.background = "cadetblue"; 
-              break;
-              case 3:
-                playerUI.childNodes[1].style.background = "cadetblue";
-                playerUI.childNodes[3].style.background = "cadetblue";
-                playerUI.childNodes[5].style.background = "cadetblue";
-              break;
-            }
-          }
-        break;
-        case bonuses.SELF_LARGE:
-            if(play){
-                yareyaredaze.play();
+              
+            break;
+            case bonuses.ENEMY_HEAL:
+                if(play){
+                    ohno.play();
+                }
+              currBonus = bonuses.ENEMY_HEAL;
+              document.getElementById("info").textContent="Oh no! ennemy healed 1HP :("
+              console.log("da bad healu");
+              if(botH<3 && botH >0){
+                botH++;
+                switch(botH){
+                  case 2:
+                    botUI.childNodes[1].style.background = "red";
+                    botUI.childNodes[3].style.background = "cadetblue";
+                    botUI.childNodes[5].style.background = "cadetblue";
+                  break;
+                  case 3:
+                    botUI.childNodes[1].style.background = "cadetblue";
+                    botUI.childNodes[3].style.background = "cadetblue";
+                    botUI.childNodes[5].style.background = "cadetblue";
+                  break;
+                }
               }
-          document.getElementById("info").textContent="Your paddle is now larger for 5s";
-          currBonus = bonuses.SELF_LARGE;
-          scene.remove(paddle1);
-          paddle1.scale.y = 1.2;
-          scene.add(paddle1);
-        break;
-        case bonuses.ENEMY_LARGE:
-            if(play){
-                ohno.play();
-            }
-         document.getElementById("info").textContent="ennemy paddle is now  larger for 5s";
-          currBonus = bonuses.ENEMY_LARGE;
-          scene.remove(paddle2);
-          paddle2.scale.y = 1.2;
-          scene.add(paddle2);
-        break;
-        case bonuses.ENEMY_STOP:
-            if(play){
-                yareyaredaze.play();
+            break;
+            case bonuses.SELF_HEAL:
+                if(play){
+                    yareyaredaze.play();
+                  }
+              currBonus = bonuses.SELF_HEAL;
+              document.getElementById("info").textContent="Congratulations: you healed 1HP :)";
+              console.log("da good healu");
+              if(playerH< 3 && playerH>0){
+                playerH++;
+                switch (playerH){
+                  case 2:
+                    playerUI.childNodes[1].style.background = "red";
+                    playerUI.childNodes[3].style.background = "cadetblue";
+                    playerUI.childNodes[5].style.background = "cadetblue"; 
+                  break;
+                  case 3:
+                    playerUI.childNodes[1].style.background = "cadetblue";
+                    playerUI.childNodes[3].style.background = "cadetblue";
+                    playerUI.childNodes[5].style.background = "cadetblue";
+                  break;
+                }
               }
-          document.getElementById("info").textContent="ennemy stopped moving !";
-          currBonus = bonuses.ENEMY_STOP;
-          paddle2Speed= 0;
-          let T1 = setTimeout(reset_speed, 1000);
-        break;
-        case bonuses.SELF_STOP:
-            if(play){
-                ohno.play();
-            }
-            document.getElementById("info").textContent="oh no ! you stopped moving";
-          currBonus = bonuses.SELF_STOP;
-          paddle1speed=0;
-          let T2= setTimeout(reset_speed, 1000);
-        break;
-        case bonuses.ZA_WARUDO:
-          if(play){
-              toggle_music();
-              za_warudo.play();
+            break;
+            case bonuses.SELF_LARGE:
+                if(play){
+                    yareyaredaze.play();
+                  }
+              document.getElementById("info").textContent="Your paddle is now larger for 5s";
+              currBonus = bonuses.SELF_LARGE;
+              scene.remove(paddle1);
+              paddle1.scale.y = 1.2;
+              scene.add(paddle1);
+            break;
+            case bonuses.ENEMY_LARGE:
+                if(play){
+                    ohno.play();
+                }
+             document.getElementById("info").textContent="ennemy paddle is now  larger for 5s";
+              currBonus = bonuses.ENEMY_LARGE;
+              scene.remove(paddle2);
+              paddle2.scale.y = 1.2;
+              scene.add(paddle2);
+            break;
+            case bonuses.ENEMY_STOP:
+                if(play){
+                    yareyaredaze.play();
+                  }
+              document.getElementById("info").textContent="ennemy stopped moving !";
+              currBonus = bonuses.ENEMY_STOP;
+              paddle2Speed= 0;
+              let T1 = setTimeout(reset_speed, 1000);
+            break;
+            case bonuses.SELF_STOP:
+                if(play){
+                    ohno.play();
+                }
+                document.getElementById("info").textContent="oh no ! you stopped moving";
+              currBonus = bonuses.SELF_STOP;
+              paddle1speed=0;
+              let T2= setTimeout(reset_speed, 1000);
+            break;
+            case bonuses.ZA_WARUDO:
+              if(play){
+                  toggle_music();
+                  za_warudo.play();
+              }
+              currBonus = bonuses.ZA_WARUDO;
+              paddle12peed=0;
+              paddle2Speed=0;
+              let T3 = setTimeout(reset_speed, 1000);
+            break;
+            default:
+                ("je sais pas ce bonus :", currBonus);
+            break;
+        
           }
-          currBonus = bonuses.ZA_WARUDO;
-          paddle12peed=0;
-          paddle2Speed=0;
-          let T3 = setTimeout(reset_speed, 1000);
-        break;
-        default:
-            ("je sais pas ce bonus :", currBonus);
-        break;
-    
-      }
+    }
+   
  }
 
  function confine(){
